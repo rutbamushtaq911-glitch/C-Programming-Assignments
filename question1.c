d84c34a2179e766826fdae7a618918cd6670cba4
Rutba <rutbamushtaq911@gmail.com>
2026-04-09 20:09:59 +0530

    Added sum program

---
+++ question1.c
@@ -0,0 +1,10 @@
+// addition of two numbers.
+#include<stdio.h>
+int main(){
+    int a,b,sum;
+    printf("enter the two numbers :");
+    scanf("%d %d",&a,&b);
+    sum = a+b;
+    printf("the sum is %d",sum);
+    return 0;
+} 
