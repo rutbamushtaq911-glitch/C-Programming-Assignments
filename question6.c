47062d7758e6ec6d22446a79ef46c888ce96b5f0
Rutba <rutbamushtaq911@gmail.com>
2026-04-10 12:14:45 +0530

    inverted triangle

--- question6.c
+++ question6.c
@@ -4,14 +4,14 @@ int main(){
     for(i=1;i<=5;i++){
         for(j=1;j<=n-i;j++){
             printf(" ");}
-        for(j=1;j<=2*i-1;j++){
+        for(j=1;j<=2*i-1;j++){      // upper half of diamond
             printf("*");}
             printf("\n");}
             
         
     for(i=n-1;i>=1;i--){
         for(j=1;j<=n-i;j++){
-            printf(" ");}
+            printf(" ");}          // lower half of diamond
         for(j=1;j<=2*i-1;j++){
             printf("*");}
             printf("\n");}
