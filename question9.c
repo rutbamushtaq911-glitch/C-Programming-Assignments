08ed27726894918aeb9726386a4b87cbe255dd1b
Rutba <rutbamushtaq911@gmail.com>
2026-04-23 22:33:37 +0530

    square series

---
+++ question9.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int n,i,sum,j ;
+    sum = 0;
+    printf(" enter the number :");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        j = i*i;
+        sum = sum + j;}
+        printf(" the sum is %d",sum);
+        
+    
+    return 0;}
+
+
+
+
+
+
+
+
+
+
