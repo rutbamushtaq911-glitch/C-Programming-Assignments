ee2606b3f7cd5a96900048a9a12153aa8f6a6248
Rutba <rutbamushtaq911@gmail.com>
2026-04-24 12:12:03 +0530

    reverse of an array

---
+++ question13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int i,n;
+printf("enter no. of elements");
+scanf("%d",&n);
+int arr[n];
+printf(" enter the elements : \n");
+for(i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+}
+printf(" the reversed array is:");
+for(i=n-1;i>=0;i--){
+    printf(" %d",arr[i]);}
+    return 0;
+}
